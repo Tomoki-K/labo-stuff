d433d6eb32df84268ea9a81f07c680b8cf6bc033
Tomoki-K <tomofuma@gmail.com>
2019-01-05 13:44:34 +0900

    actually create mutant file

--- mutate.rb
+++ mutate.rb
@@ -1,36 +1,59 @@
 require "./mutation_patterns.rb"
 require "./reader.rb"
-require 'pry'
+require "./writer.rb"
 
 INPUTFILE = "./test/sample.c"
+OUTPUTDIR = "./test/mutants"
 
-reader = Reader.new(INPUTFILE)
-source_code = reader.read_lines
+def main
+  reader = Reader.new(INPUTFILE)
+  source_code = reader.read_lines
 
-mutated_line = ""
-source_code.each_with_index do |line, idx|
-  # do not mutate preprocessor or assert statements
-  next if line.strip.start_with?("#") || line.strip.start_with?("assert")
+  mutant_count = 0
+  source_code.each_with_index do |line, line_num|
+    # do not mutate preprocessor or assert statements
+    next if line.strip.start_with?("#") || line.strip.start_with?("assert")
 
-  MUTATION_PATTERNS.each do |pattern, mutations|
-    # search for mutatable substring
-    mutatable_substring_count = line.scan(/(?=#{Regexp.quote(pattern.to_s)})/).count
-    next if mutatable_substring_count == 0
+    MUTATION_PATTERNS.each do |pattern, mutations|
+      # search for mutatable substring
+      mutatable_substring_count = line.scan(/(?=#{Regexp.quote(pattern.to_s)})/).count
+      next if mutatable_substring_count == 0
 
-    # mutate each substring if multiple are found
-    mutate_at_index = 0
-    for r in 1..mutatable_substring_count do
-      mutate_at_index = line.index(pattern.to_s, mutate_at_index == 0 ? 0 : mutate_at_index + 1)
+      # mutate each substring if multiple are found
+      mutate_at_index = 0
+      for r in 1..mutatable_substring_count do
+        mutate_at_index = line.index(pattern.to_s, mutate_at_index == 0 ? 0 : mutate_at_index + 1)
 
-      # create mutant for each way of mutating the substring
-      mutations.each do |mutation|
-        mutated_line = line[0..mutate_at_index - 1] + line[mutate_at_index..].sub(pattern.to_s, mutation[:substring])
-        # TODO: create mutant
-        puts "\n==> @Line: #{idx + 1}:#{mutate_at_index} type: #{mutation[:type]}"
-        puts "Original Line: #{line.strip}"
-        puts "Mutated Line : #{mutated_line.strip}"
+        # create mutant for each way of mutating the substring
+        mutations.each do |mutation|
+          mutated_line = line[0..mutate_at_index - 1] + line[mutate_at_index..].sub(pattern.to_s, mutation[:substring])
+          mutant_info = [
+            "==> @Line: #{line_num + 1}:#{mutate_at_index} type: #{mutation[:type]}",
+            "Original Line: #{line.strip}",
+            "Mutated Line : #{mutated_line.strip}"
+          ]
+          create_mutant("#{OUTPUTDIR}/mutant_#{mutant_count}.c",
+                        source_code, line_num, mutated_line, mutant_info)
+          mutant_count += 1
+        end
       end
     end
   end
+  puts "\n============="
+  puts "#{mutant_count} mutants generated"
 end
 
+def create_mutant( filename, source_code, line_num, mutated_line, comment = [] )
+  source_code[line_num] = mutated_line
+  content = []
+  content = comment.map{|line| "// #{line}"}
+  content += [ "" ]
+  content += source_code
+  writer = Writer.new(filename)
+  writer.write(content)
+  # console output
+  puts "\n#{filename}"
+  puts comment
+end
+
+main
--- writer.rb
+++ writer.rb
@@ -1,26 +1,37 @@
 require "csv"
+require "fileutils"
+require "pry"
 
 class Writer
-  def initialize(output_file, header)
+  def initialize(output_file)
+    dirname = File.dirname(output_file)
+    unless File.directory?(dirname)
+      FileUtils.mkdir_p(dirname)
+    end
     @output_file = output_file
-    @header = header
   end
 
-  def get_header_ordered_array(row_hash)
+  def get_header_ordered_array(header, row_hash)
     row_array = []
-    @header.each do |col|
+    header.each do |col|
       row_array.push(row_hash[col.to_sym])
     end
     row_array
   end
 
-  def write_csv(results_arr)
+  def write_csv(header, results_arr)
     CSV.open(@output_file, 'w') do |csv|
-      csv << @header
+      csv << header
       results_arr.each do |result|
-        csv << get_header_ordered_array(result)
+        csv << get_header_ordered_array(header, result)
       end
       csv.close
+    end
+  end
+
+  def write(content_array)
+    File.open(@output_file, 'w') do |f|
+      f.puts(content_array)
     end
   end
 end
