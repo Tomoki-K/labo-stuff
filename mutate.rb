2b9e83de745634bedff34d5326fbdf6800c3559a
Tomoki-K <tomofuma@gmail.com>
2019-01-05 12:38:50 +0900

    add mutation type info

--- mutate.rb
+++ mutate.rb
@@ -12,23 +12,23 @@ source_code.each_with_index do |line, idx|
   # do not mutate preprocessor or assert statements
   next if line.strip.start_with?("#") || line.strip.start_with?("assert")
 
-  MUTATION_PATTERNS.keys.each do |mo|
+  MUTATION_PATTERNS.each do |pattern, mutations|
     # search for mutatable substring
-    mutatable_substring_count = line.scan(/(?=#{Regexp.quote(mo.to_s)})/).count
+    mutatable_substring_count = line.scan(/(?=#{Regexp.quote(pattern.to_s)})/).count
     next if mutatable_substring_count == 0
 
     # mutate each substring if multiple are found
     mutate_at_index = 0
     for r in 1..mutatable_substring_count do
-      mutate_at_index = line.index(mo.to_s, mutate_at_index == 0 ? 0 : mutate_at_index + 1)
+      mutate_at_index = line.index(pattern.to_s, mutate_at_index == 0 ? 0 : mutate_at_index + 1)
 
       # create mutant for each way of mutating the substring
-      MUTATION_PATTERNS[mo].each do |mutate_with|
-        mutated_line = line[0..mutate_at_index - 1] + line[mutate_at_index..].sub(mo.to_s, mutate_with)
+      mutations.each do |mutation|
+        mutated_line = line[0..mutate_at_index - 1] + line[mutate_at_index..].sub(pattern.to_s, mutation[:substring])
         # TODO: create mutant
-        puts "\n==> @Line: #{idx + 1}:#{mutate_at_index}"
+        puts "\n==> @Line: #{idx + 1}:#{mutate_at_index} type: #{mutation[:type]}"
         puts "Original Line: #{line.strip}"
-        puts "Mutated Line:  #{mutated_line.strip}"
+        puts "Mutated Line : #{mutated_line.strip}"
       end
     end
   end
--- mutation_patterns.rb
+++ mutation_patterns.rb
@@ -1,32 +1,60 @@
 NULL_STRING = " "
-MUTATION_PATTERNS = {
-  " < ": [ " != ", " > ", " <= ", " >= ", " == " ],
-  " > ": [ " != ", " < ", " <= ", " >= ", " == " ],
-  "<=": [ " != ", " < ", " > ", " >= ", "==" ],
-  ">=": [ " != ", " < ", " <= ", " > ", "==" ],
-  "==": [ " != ", " = ", " < ", " > ", " <= ", " >= " ],
-  "!=": [ " == ", " = ", " < ", " > ", " <= ", " >= " ],
-  " = ": [ " == ", " != ", " < ", " > ", " <= ", " >= ", " = 0 * ", " = 0 ;//", " = NULL; //", " = ! " ],
-  " + ": [ " - ", " * ", " / ", " % " ],
-  " - ": [ " + ", " * ", " / ", " % " ],
-  " * ": [ " + ", " - ", " / ", " % " ],
-  " / ": [ " % ", " * ", " + ", " - " ],
-  " % ": [ " / ", " + ", " - ", " * " ],
-  " + 1": [ " - 1", "+ 0", "+ 2", "- 2" ],
-  " - 1": [ " + 1", "+ 0", "+ 2", "- 2" ],
-  " & ": [ " | ", " ^ " ],
-  " | ": [ " & ", " ^ " ],
-  " ^ ": [ " & ", " | " ],
-  " ~": [ " !", NULL_STRING ],
-  " !": [ " ~", NULL_STRING ],
-  " && ": [ " & ", " || "," && !" ],
-  " || ": [ " | ", " && ", " || !" ],
-  " >> ": [ " << " ],
-  " << ": [ " >> " ],
-  " << 1": [ " << 0"," << -1", "<< 2" ],
-  " >> 1": [ " >> 0", " >> -1", ">> 2" ],
-  "++": [ "--" ],
-  "--": [ "++" ],
-  " true ": [ " false " ],
-  " false ": [ " true " ],
+
+# ref: https://www.tutorialspoint.com/cprogramming/c_operators.htm
+
+MUTATION_TYPES = {
+  # ABS: Absolute value insertion
+  "ABS": {
+    " = ": [ " = 0 * ", " = 0 ;//", " = NULL; //" ],
+    " + 1": [ "+ 0", "+ 2" ],
+    " - 1": [ "+ 0", "- 2" ],
+    " true ": [ " false " ],
+    " false ": [ " true " ],
+  },
+  # AOR: Arithmic operator replacement
+  "AOR": {
+    " + ": [ " - ", " * ", " / ", " % " ],
+    " - ": [ " + ", " * ", " / ", " % " ],
+    " * ": [ " + ", " - ", " / ", " % " ],
+    " / ": [ " % ", " * ", " + ", " - " ],
+    " % ": [ " / ", " + ", " - ", " * " ],
+  },
+  # ROR: Relational operator replacement
+  "ROR": {
+    " < ": [ " != ", " > ", " <= ", " >= ", " == " ],
+    " > ": [ " != ", " < ", " <= ", " >= ", " == " ],
+    "<=": [ " != ", " < ", " > ", " >= ", "==" ],
+    ">=": [ " != ", " < ", " <= ", " > ", "==" ],
+    "==": [ " != ", " < ", " > ", " <= ", " >= " ],
+    "!=": [ " == ", " < ", " > ", " <= ", " >= " ],
+  },
+  # LOR: Logical operator replacement
+  "LOR": {
+    " !": [ NULL_STRING ],
+    " && ": [ " || ", " && !" ],
+    " || ": [ " && ", " || !" ],
+  },
+  # ASR: Assignment operator replacement
+  "ASR": {
+    " = ": [ " += ", " -= ", " *= ", " /= ", " %= " ],
+  },
+  # UOI: Unary operator insertion
+  "UOI": {
+    "++": [ "--" ],
+    "--": [ "++" ],
+  },
 }.freeze
+
+def construct_mutation_patterns
+  patterns = {}
+  MUTATION_TYPES.each do |type, mutators|
+    mutators.each do |target, replacements|
+      # create mutation object for each mutation pattern
+      replacements.map! {|r|{ type: type.to_s, substring: r } }
+      patterns[target] = patterns.key?(target) ? (patterns[target] + replacements).uniq : replacements
+    end
+  end
+  return patterns
+end
+
+MUTATION_PATTERNS = construct_mutation_patterns.freeze
